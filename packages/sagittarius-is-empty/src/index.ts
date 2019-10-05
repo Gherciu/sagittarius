b3b2d21fd2a64131500427855546143d9867e189
happy_hunter <gherciu553@gmail.com>
2019-10-05 22:45:46 +0300

    fix(sagittarius-is-empty): incorect return

--- packages/sagittarius-is-empty/src/index.ts
+++ packages/sagittarius-is-empty/src/index.ts
@@ -1,9 +1,9 @@
 import { TIsEmpty } from '../@types'
 
 const isEmpty: TIsEmpty = arg => {
-  if (Array.isArray(arg)) return Boolean(arg.length)
-  if (typeof arg === 'object') return Boolean(Object.keys(arg).length)
-  return Boolean(arg)
+  if (Array.isArray(arg)) return !arg.length
+  if (typeof arg === 'object') return !Object.keys(arg).length
+  return !arg
 }
 
 export default isEmpty
