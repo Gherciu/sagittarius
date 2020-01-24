2f4c7bdb7b74a8b0e576671ebce44f7485baebd4
Gherciu Gheorghe <gheorghe.gherciu@endava.com>
2020-01-24 11:43:08 +0200

    Deploy website version based on 438ab4d76cdc34e4ccb1e0f4b06de0d0c10faa3c

---
+++ d0a99404.33d9e69e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{148:function(a){a.exports=JSON.parse('{"permalink":"/sagittarius/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null}')}}]);
--- d0a99404.50345a21.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{111:function(a){a.exports=JSON.parse('{"permalink":"/sagittarius/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null}')}}]);
