487764d83b6658e3083fb28d636b7d0512989596
Gherciu Gheorghe <gheorghe.gherciu@endava.com>
2020-01-24 13:30:52 +0200

    fix(uniq): stringify only if value has type object

--- packages/sagittarius-uniq/src/index.ts
+++ packages/sagittarius-uniq/src/index.ts
@@ -2,8 +2,10 @@ import { TUniq } from '../@types'
 
 const uniq: TUniq = (arg = []) => {
   const stringifyedArg = arg.map(value => JSON.stringify(value))
-  return arg.filter((value, index) => {
-    return stringifyedArg.indexOf(JSON.stringify(value)) === index
+  return arg.filter((value, index, self) => {
+    if (typeof value === 'object')
+      return stringifyedArg.indexOf(JSON.stringify(value)) === index
+    return self.indexOf(value) === index
   })
 }
 
--- packages/sagittarius-uniq/src/tests/index.test.ts
+++ packages/sagittarius-uniq/src/tests/index.test.ts
@@ -8,10 +8,27 @@ describe('uniq', () => {
   it('should return filtered uniq values', () => {
     expect(uniq([21, 'twenty one', 21])).toEqual([21, 'twenty one'])
     expect(uniq([21, 'twenty one', null, 21])).toEqual([21, 'twenty one', null])
-    expect(uniq([21, 'twenty one', 21, [21, 'twenty one', 21]])).toEqual([
+    expect(uniq([21, 'twenty one', null, 'null', 21])).toEqual([
+      21,
+      'twenty one',
+      null,
+      'null',
+    ])
+    expect(uniq([21, 'twenty one', 21, '21'])).toEqual([21, 'twenty one', '21'])
+    expect(
+      uniq([
+        21,
+        'twenty one',
+        21,
+        [21, 'twenty one', 21],
+        [21, 'twenty one', 21],
+        [21, 'twenty one', '21'],
+      ]),
+    ).toEqual([
       21,
       'twenty one',
       [21, 'twenty one', 21],
+      [21, 'twenty one', '21'],
     ])
     expect(uniq([{ a: 21 }, { a: 'twenty one' }, { a: 21 }])).toEqual([
       { a: 21 },
